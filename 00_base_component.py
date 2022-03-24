e09528727f1bac5bad5317a697302ae62c3f68e4
cmcla003 <mclarenc@awatapu.school.nz>
2022-03-25 10:55:53 +1300

    added score, corrects comparisons

--- 00_base_component.py
+++ 00_base_component.py
@@ -40,6 +40,8 @@ def check_rounds(question, error):
 yes_no_list = ["yes", "no"]
 rps_list = ["rock","paper","scissors","exit"]
 rounds_played = 0
+rounds_lost = 0
+rounds_draw = 0
 # Ask user if played before
 
 # If no show instructions
@@ -68,16 +70,21 @@ while end_game == "no":
     comp_choice = random.choice(rps_list[:-1])
 
     # compare user choice with computer choice
-    if user_choice == comp_choice:
+    if user_choice == comp_choice[0]:
         result = "Draw"
-    elif user_choice == "rock" and comp_choice == "scissors":
+        rounds_draw += 1
+    elif user_choice == "rock" or user_choice =="r" and comp_choice == "scissors":
         result = "Win"
-    elif user_choice == "paper" and comp_choice == "rock":
+    elif user_choice == "paper" or user_choice =="p" and comp_choice == "rock":
         result = "Win"
-    elif user_choice == "scissors" and comp_choice == "paper":
+    elif user_choice == "scissors" or user_choice =="s" and comp_choice == "paper":
         result = "Win"
+    # exit code to end program early / if in continuous mode
+    elif user_choice == "exit":
+        break
     else:
         result = "You lose"
+        rounds_lost += 1
 
     print("You chose {}, Computer chose {}. "
           "\nResult: {}".format(user_choice, comp_choice, result))
@@ -91,9 +98,16 @@ while end_game == "no":
     print()
     rounds_played +=1
 
+    # calculate numbers of win
+    rounds_won = rounds_played - rounds_lost - rounds_draw
+
+
 print("Thanks for playing")
 
 # Ask user is they want to see their game history
 # If yes show history
 
 # Show game statistics
+print()
+print("**** End Game Summary ***")
+print("Won: {} \t | \t Lost: {} \t | \t Draw: {}".format(rounds_won,rounds_lost,rounds_draw))
